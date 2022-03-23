20a255025613d284bea691cd5873f9a1030e2f2e
Yuri Levchenko <yuri.levchenko@student.tuke.sk>
2022-03-23 20:20:02 +0100

    Minor changes and fixes and comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,12 @@
 package com.company;
 
-import com.company.consoleui.ConsoleUI;
-
-import java.awt.*;
-import java.nio.channels.Pipe;
-import java.util.*;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
 
     public static void main(String[] args) {
 
-//        Game game = new Game();
-//        game.play();
+        Game game = new Game();
+        game.play();
 
-        ConsoleUI test = new ConsoleUI(3);
-        test.play();
     }
 
 
